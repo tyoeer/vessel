40a750083ed581d4d5282376fb0b10b36558649e
tyoeer <tyoeer@users.noreply.github.com>
2024-11-06 18:42:27 +0100

    Properly sync user vessel with server vessel

--- src/multiplayer.rs
+++ src/multiplayer.rs
@@ -4,8 +4,7 @@ use bevy::{color::palettes::css, prelude::*};
 use bevy_replicon::prelude::*;
 use serde::{Serialize, Deserialize};
 use crate::worldplay::{
-	self,
-	vessel,
+	self, user, vessel
 };
 
 pub struct MultiplayerPlugin;
@@ -13,10 +12,13 @@ pub struct MultiplayerPlugin;
 impl Plugin for MultiplayerPlugin {
 	fn build(&self, app: &mut App) {
 		app
-			.init_resource::<ClientEntities>()
+			.init_resource::<ClientOwnedEntities>()
 			
 			.replicate_group::<(MultiPlayer, Transform)>()
 			.add_client_event::<vessel::Control>(ChannelKind::Ordered)
+			.add_client_event::<NewUserVessel>(ChannelKind::Unordered)
+			
+			.add_systems(OnEnter(crate::GameState::WorldPlay), send_user_vessel.after(user::spawn_user).run_if(client_connected))
 			
 			.add_systems(Update, mark_players.after(vessel::move_vessel))
 			.add_systems(Update, apply_client_movement
@@ -40,7 +42,7 @@ impl Plugin for MultiplayerPlugin {
 
 ///Maps [ClientId]s to the [Entity] they're controlling
 #[derive(Resource, Default)]
-pub struct ClientEntities {
+pub struct ClientOwnedEntities {
 	pub map: HashMap<ClientId, Entity>,
 }
 
@@ -61,7 +63,7 @@ pub fn send_movement(
 pub fn apply_client_movement(
 	mut query: Query<&mut vessel::Control>,
 	mut events: EventReader<FromClient<vessel::Control>>,
-	client_entities: Res<ClientEntities>,
+	client_entities: Res<ClientOwnedEntities>,
 ) {
 	for event in events.read() {
 		let target = client_entities.map.get(&event.client_id).expect("client sending us events should have a related entity");
@@ -71,24 +73,48 @@ pub fn apply_client_movement(
 }
 
 
+#[derive(Event, Clone, Debug, serde::Serialize, serde::Deserialize)]
+pub struct NewUserVessel {
+	///The entity id of the prespawned id of the entity
+	client_entity: Entity
+}
+
+
+pub fn send_user_vessel(
+	local_vessel_query: Query<Entity, With<user::LocallyControlled>>,
+	mut events: EventWriter<NewUserVessel>,
+) {
+	let id = local_vessel_query.single();
+	events.send(NewUserVessel {
+		client_entity: id,
+	});
+}
+
+
 pub fn spawn_player(
 	mut cmds: Commands,
-	mut ev: EventReader<ServerEvent>,
-	mut client_entities: ResMut<ClientEntities>,
+	mut user_events: EventReader<FromClient<NewUserVessel>>,
+	mut client_owned_entities: ResMut<ClientOwnedEntities>,
 	rt_vessel_data: Res<vessel::RtVesselData>,
+	mut client_entity_map: ResMut<ClientEntityMap>,
 	mut spawn_events: EventWriter<vessel::SpawnEvent>,
 ) {
-	for event in ev.read() {
-		if let ServerEvent::ClientConnected { client_id } = event {
-			let id = cmds.spawn(MultiPlayer)
-				.insert(Replicated)
-				.id();
-			client_entities.map.insert(*client_id, id);
-			spawn_events.send(vessel::SpawnEvent {
-				rt_vessel_data: rt_vessel_data.clone(),
-				player_entity: Some(id),
-			});
-		}
+	for event in user_events.read() {
+		let id = cmds.spawn(MultiPlayer)
+			.insert(Replicated)
+			.id();
+		
+		spawn_events.send(vessel::SpawnEvent {
+			rt_vessel_data: rt_vessel_data.clone(),
+			player_entity: Some(id),
+		});
+		
+		client_entity_map.insert(event.client_id, ClientMapping {
+			server_entity: id,
+			client_entity: event.event.client_entity,
+		});
+		
+		client_owned_entities.map.insert(event.client_id, id);
 	}
 }
 
