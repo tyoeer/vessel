67ddc73104fd3fbac53a49fd4feddf9b78dab362
tyoeer <tyoeer@users.noreply.github.com>
2024-11-18 17:41:13 +0100

    Add SimVessel in vessel building

--- src/vessel_builder.rs
+++ src/vessel_builder.rs
@@ -9,14 +9,16 @@ Vessel pipeline:
 
 */
 
-use bevy::prelude::{
-	Transform,
-	Res,
-	Commands,
-};
+use avian3d::prelude::Collider;
+use bevy::{math::{Quat, Vec3}, prelude::{
+	Commands, Res, Transform
+}};
 
 use crate::{
-	editor::misc::CreationData,
+	editor::{
+		misc::CreationData,
+		object::Catalogue,
+	},
 	worldplay::vessel::{
 		RtVesselData,
 		VesselGraphicPart,
@@ -26,10 +28,69 @@ use crate::{
 
 
 pub fn build_vessel_system(
-	sv: Res<CreationData>,
+	creation: Res<CreationData>,
+	catalogue: Res<Catalogue>,
 	mut cmds: Commands,
 ) {
-	cmds.insert_resource(build_vessel(&sv));
+	let sim = build_sim_vessel(&creation);
+	let rt = sim_to_rt(&sim, &catalogue);
+	cmds.insert_resource(rt);
+}
+
+
+///Serializable form of a vessel meant to be played
+pub struct SimVessel {
+	/// list of (element id, where to place it)
+	graphics: Vec<(String, Transform)>,
+	collider: avian3d::collision::Collider,
+	physics_properties: VesselProperties
+}
+
+
+pub fn build_sim_vessel(
+	creation: &CreationData,
+) -> SimVessel {
+	let mut graphics = Vec::new();
+	let mut collider_parts: Vec<(Vec3, Quat, Collider)> = Vec::new();
+	
+	for object in &creation.objects {
+		let pos = object.pos.0;
+		let transform = Transform::from_translation(pos.as_vec3());
+		
+		collider_parts.push((
+			pos.as_vec3(),
+			Quat::default(),
+			object.element.collider.clone(),
+		));
+		graphics.push((object.element.id.clone(), transform));
+	}
+	
+	SimVessel {
+		graphics,
+		collider: Collider::compound(collider_parts),
+		physics_properties: VesselProperties::default(),
+	}
+}
+
+
+pub fn sim_to_rt(
+	sim: &SimVessel,
+	catalogue: &Catalogue,
+) -> RtVesselData {
+	let graphics = sim.graphics.iter()
+		.map(|(elem_id, transform)| {
+			let elem = catalogue.find_by_id(elem_id);
+			VesselGraphicPart {
+				mesh: elem.graphics.mesh.clone(),
+				material: elem.graphics.material.clone(),
+				transform: *transform,
+			}
+		}).collect();
+		
+	RtVesselData {
+		graphics,
+		vessel_info: sim.physics_properties.clone(),
+	}
 }
 
 
