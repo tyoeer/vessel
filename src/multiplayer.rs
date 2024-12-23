01722b30fe616a484f5758bbd0f21f5ea4562ffe
tyoeer <tyoeer@users.noreply.github.com>
2024-12-23 22:17:03 +0100

    Add network debug UI

--- src/multiplayer.rs
+++ src/multiplayer.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
 
 use avian3d::prelude::{AngularVelocity, LinearVelocity, Position, Rotation};
-use bevy::{color::palettes::css, prelude::*};
-use bevy_replicon::prelude::*;
+use bevy::{color::palettes::css, ecs::entity::EntityHashMap, prelude::*, window::PrimaryWindow};
+use bevy_replicon::{core::server_entity_map::ServerEntityMap, prelude::*};
 use serde::{Serialize, Deserialize};
 use crate::worldplay::{
 	self, user, vessel,
@@ -23,7 +23,7 @@ impl Plugin for MultiplayerPlugin {
 			.add_systems(OnEnter(crate::GameState::WorldPlay), send_user_vessel.after(user::spawn_user).run_if(client_connected))
 		;
 		
-		app.add_plugins(bevy_inspector_egui::quick::FilterQueryInspectorPlugin::<With<MultiPlayer>>::new());
+		// app.add_plugins(bevy_inspector_egui::quick::FilterQueryInspectorPlugin::<With<MultiPlayer>>::new());
 		/*
 		app.add_plugins(bevy_inspector_egui::quick::FilterQueryInspectorPlugin::<(
 			With<MultiPlayer>,
@@ -37,6 +37,7 @@ impl Plugin for MultiplayerPlugin {
 		#[cfg(feature="user_interface")]
 		app
 			.add_systems(Update, mark_players.after(vessel::move_vessel))
+			.add_systems(Update, debug_ui)
 			.add_systems(
 				OnEnter(crate::GameState::WorldPlay),
 				mark_server_user.after(user::spawn_user).run_if(server_running)
@@ -61,6 +62,81 @@ impl Plugin for MultiplayerPlugin {
 			.add_systems(PostUpdate, send_movement.before(ClientSet::Send).run_if(client_connected))
 		;
 	}
+}
+
+
+pub fn debug_ui(
+	world: &mut World,
+) {
+	use bevy_egui::{EguiContext, egui};
+	// use bevy_inspector_egui as bie;
+	// fn entity_query_ui<F: QueryFilter>(world: &mut World) {
+	let egui_context = world
+		.query_filtered::<&mut EguiContext, With<PrimaryWindow>>()
+		.get_single(world);
+
+	let Ok(egui_context) = egui_context else {
+		return;
+	};
+	let mut egui_context = egui_context.clone();
+	
+	#[derive(Default)]
+	struct EntityInfo {
+		replicated: bool,
+		group: bool,
+		//contains server entity id if present
+		mapping: Option<Entity>,
+	}
+	
+	let mut infos = EntityHashMap::<EntityInfo>::default();
+	
+	for replicated in world.query_filtered::<Entity, With<Replicated>>().iter(world) {
+		infos.entry(replicated).or_default().replicated = true;
+	}
+	
+	world.query_filtered::<Entity, (
+		With<MultiPlayer>,
+		With<vessel::Id>,
+		With<Position>,
+		With<Rotation>,
+		With<LinearVelocity>,
+		With<AngularVelocity>,
+	)>()
+		.iter(world)
+		.for_each(|entity| {infos.entry(entity).or_default().group = true});
+	
+	let maybe_map = world.get_resource::<ServerEntityMap>().map(|map| map.to_server().clone());
+	if let Some(map) = maybe_map {
+		for (client,server) in map.iter() {
+			infos.entry(*client).or_default().mapping = Some(*server);
+		}
+	}
+	
+	
+	egui::Window::new("Multiplayer entities")
+		.default_size((400., 160.))
+		.show(egui_context.get_mut(), |ui| {
+			egui::ScrollArea::both().show(ui, |ui| {
+				for (entity, info) in infos.iter() {
+					let name = bevy_inspector_egui::bevy_inspector::guess_entity_name(world, *entity);
+					let label = format!(
+						"{}{}{} {}{}",
+						if info.replicated {'R'} else {'_'},
+						if info.group {'G'} else {'.'},
+						if info.mapping.is_some() {'M'} else {'.'},
+						name,
+						info.mapping.map(|se| format!(" server: {se}")).unwrap_or("".into()),
+					);
+					ui.label(egui::RichText::new(label).font(egui::FontId::monospace(12.)));
+					// egui::CollapsingHeader::new(name).show(ui, |ui| {
+					// 	bie::bevy_inspector::ui_for_entity(world, entity, ui);
+					// });
+				}
+				ui.allocate_space(ui.available_size());
+			});
+		})
+	;
+	
 }
 
 
