f2be641e443b5b8e89bd4bf2f03812f79f140097
tyoeer <tyoeer@users.noreply.github.com>
2025-01-03 23:41:47 +0100

    Fix the editor camera

--- src/editor/input.rs
+++ src/editor/input.rs
@@ -16,22 +16,16 @@ pub fn move_camera(
 ) {
 	if !mouse_buttons.pressed(MouseButton::Right) {return}
 	
-	for mut tf in &mut camera_transforms {
-		
-		//The camera's mapping from world space to window/viewport space is weird
-		// and the documentation appears to be wrong
-		// The correctness of the following code has been determined empirically by testing
-		
+	for mut tf in &mut camera_transforms {		
 		for ev in mouse_motion_events.read() {
 			tf.rotate_y(-ev.delta.x * SENSITIVITY);
-			tf.rotate_local_x(ev.delta.y * SENSITIVITY);
+			tf.rotate_local_x(-ev.delta.y * SENSITIVITY);
 		}
 		//make sure Y stays up
 		let forward = tf.forward();
-		//camera y goes down, so we need to flip it to make world Y render upwards
-		tf.look_to(forward, -Dir3::Y);
+		tf.look_to(forward, Dir3::Y);
 		
-		//camera: x+ = left, y+ = down, z+ = back
+		//camera: x+ = right, y+ = up, z+ = back
 		let mut local_offset = Vec3::ZERO;
 		if key_codes.pressed(KeyCode::KeyW) {
 			local_offset.z -= 1.;
@@ -40,35 +34,20 @@ pub fn move_camera(
 			local_offset.z += 1.;
 		}
 		if key_codes.pressed(KeyCode::KeyE) {
-			local_offset.y -= 1.;
-		}
-		if key_codes.pressed(KeyCode::KeyQ) {
 			local_offset.y += 1.;
 		}
+		if key_codes.pressed(KeyCode::KeyQ) {
+			local_offset.y -= 1.;
+		}
 		if key_codes.pressed(KeyCode::KeyD) {
-			local_offset.x -= 1.;
+			local_offset.x += 1.;
 		}
 		if key_codes.pressed(KeyCode::KeyA) {
-			local_offset.x += 1.;
+			local_offset.x -= 1.;
 		}
 		
 		let offset = tf.rotation.mul_vec3(local_offset * MOVE_SPEED * timer.delta_secs());
 		tf.translation += offset;
-		
-		// const SIZE: f32 = 0.3;
-		// use bevy::color::palettes::css::{BLACK, BLUE, GREEN, RED};
-		
-		// let pos = tf.translation + tf.forward()*5.;
-		// if kcs.pressed(KeyCode::KeyR) {
-		// 	gizmos.line(pos, pos+tf.local_x()*SIZE, RED);
-		// 	gizmos.line(pos, pos+tf.local_y()*SIZE, GREEN);
-		// 	gizmos.line(pos, pos+tf.local_z()*SIZE, BLUE);
-		// } else {
-		// 	gizmos.line(pos, pos+Vec3::X*SIZE, RED);
-		// 	gizmos.line(pos, pos+Vec3::Y*SIZE, GREEN);
-		// 	gizmos.line(pos, pos+Vec3::Z*SIZE, BLUE);
-		// }
-		// gizmos.sphere(Vec3::ZERO, Quat::default(), 1., BLACK);
 	}
 }
 
