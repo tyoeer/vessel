356a1419e55687c07fc02b0f998a57d4ffe8e176
tyoeer <tyoeer@users.noreply.github.com>
2025-01-04 21:50:08 +0100

    Multiplayer debug UI:
    - Add replication groups UI
    - Check groups at runtime instead of hardcoded components

--- src/multiplayer/ui.rs
+++ src/multiplayer/ui.rs
@@ -1,17 +1,40 @@
-use bevy::prelude::*;
-use bevy_replicon::prelude::*;
+use bevy::{
+	prelude::*,
+	ecs::component::ComponentId,
+};
+use bevy_replicon::{
+	prelude::*,
+	core::replication::replication_rules::ReplicationRules,
+};
 use bevy_egui::egui::*;
 
 use super::*;
 
 
+pub const FONT_SIZE: f32 = 12.;
+
+
+
+pub fn collect_replication_groups(world: &World) -> Vec<Vec<ComponentId>> {
+	let rules = world.resource::<ReplicationRules>();
+	rules.iter()
+		.map(|rule| &rule.components)
+		.map(|cs| cs.iter()
+			.map(|c_f| c_f.0)
+			.collect::<Vec<_>>()
+		)
+		.collect()
+}
+
+
 #[derive(Default)]
 pub struct EntityInfo {
 	pub replicated: bool,
-	pub group: bool,
+	pub groups: Vec<bool>,
 	//contains server entity id if present
 	pub mapping: Option<Entity>,
 }
+
 
 ///Collects entities related to multiplayer stuff and interesting things about them
 pub fn collect_entities(world: &mut World) -> EntityHashMap::<EntityInfo> {
@@ -20,17 +43,24 @@ pub fn collect_entities(world: &mut World) -> EntityHashMap::<EntityInfo> {
 	for replicated in world.query_filtered::<Entity, With<Replicated>>().iter(world) {
 		infos.entry(replicated).or_default().replicated = true;
 	}
-
-	world.query_filtered::<Entity, (
-		With<MultiPlayer>,
-		With<vessel::Id>,
-		With<Position>,
-		With<Rotation>,
-		With<LinearVelocity>,
-		With<AngularVelocity>,
-	)>()
-		.iter(world)
-		.for_each(|entity| {infos.entry(entity).or_default().group = true});
+	
+	let groups = collect_replication_groups(world);
+	let group_count = groups.len();
+	
+	for (i, group) in groups.into_iter().enumerate() {
+		let mut query_builder = QueryBuilder::<Entity>::new(world);
+		for comp_id in group {
+			query_builder.with_id(comp_id);
+		}
+		let mut q = query_builder.build();
+		
+		for entity in q.iter(world) {
+			let info = infos.entry(entity).or_default();
+			info.groups.resize(group_count, false);
+			info.groups[i] = true;
+		}
+	}
+	
 
 	let maybe_map = world.get_resource::<ServerEntityMap>().map(|map| map.to_server().clone());
 	if let Some(map) = maybe_map {
@@ -42,7 +72,7 @@ pub fn collect_entities(world: &mut World) -> EntityHashMap::<EntityInfo> {
 	infos
 }
 
-pub fn multiplayer_entities(ui: &mut Ui, world: &mut World) {
+pub fn multiplayer_entities_ui(ui: &mut Ui, world: &mut World) {
 	let infos = collect_entities(world);
 	
 	for (entity, info) in infos.iter() {
@@ -50,21 +80,79 @@ pub fn multiplayer_entities(ui: &mut Ui, world: &mut World) {
 		let label = format!(
 			"{}{}{} {}{}",
 			if info.replicated {'R'} else {'_'},
-			if info.group {'G'} else {'.'},
+			if info.groups.iter().any(|x| *x) {'G'} else {'.'},
 			if info.mapping.is_some() {'M'} else {'.'},
 			name,
 			info.mapping.map(|se| format!(" server: {se}")).unwrap_or("".into()),
 		);
-		ui.label(RichText::new(label).font(FontId::monospace(12.)));
+		ui.label(RichText::new(label).font(FontId::monospace(FONT_SIZE)));
 		// egui::CollapsingHeader::new(name).show(ui, |ui| {
 		// 	bie::bevy_inspector::ui_for_entity(world, entity, ui);
 		// });
 	}
 }
 
+pub fn shorten_component_name(name: &str) -> &str {
+	const SPLIT: &str = "::";
+	
+	let parts = name.split(SPLIT).collect::<Vec<_>>();
+	let last = parts.last().expect("component name should not be empty");
+	if last.len() <= 3 && parts.len() >= 2 {
+		let second_to_last = parts[parts.len()-2];
+		let idx = name.len() - second_to_last.len() - SPLIT.len() - last.len();
+		return &name[idx..];
+	}
+	
+	last
+}
+
+pub fn groups_ui(ui: &mut Ui, world: &mut World) {
+	let rules = collect_replication_groups(world);
+	for (number, rule) in rules.into_iter().enumerate() {
+		let comp_names_short = rule.iter().map(
+			|comp_id| world
+				.components()
+				.get_name(*comp_id)
+				.expect("component in replication rule should be registered with the bevy world")
+		).map(shorten_component_name).collect::<Vec<_>>().join(", ");
+		
+		let mut heading = text::LayoutJob::default();
+		heading.append(
+			&format!("{number:2}"),
+			0.,
+			TextFormat {
+				font_id: FontId::monospace(FONT_SIZE),
+				..default()
+			}
+		);
+		heading.append(
+			&comp_names_short,
+			10.,
+			TextFormat::default(),
+		);
+		ui.collapsing(heading, |ui| {
+			for component_id in rule {
+				let name = world.components()
+					.get_name(component_id)
+					.expect("component in replication rule should be registered with the bevy world");
+				ui.label(name);
+			}
+		});
+	}
+}
+
+
+#[derive(Default)]
+pub enum Tab {
+	#[default]
+	Entities,
+	Groups,
+}
+
 
 pub fn debug_ui(
 	world: &mut World,
+	mut tab: Local<Tab>,
 ) {
 	use bevy_egui::{EguiContext, egui};
 	// use bevy_inspector_egui as bie;
@@ -83,8 +171,21 @@ pub fn debug_ui(
 		.default_size((400., 160.))
 		.show(egui_context.get_mut(), |ui| {
 			egui::ScrollArea::both().show(ui, |ui| {
+				ui.horizontal(|ui| {
+					if ui.button("Entities").clicked() {
+						*tab = Tab::Entities;
+					}
+					if ui.button("Groups").clicked() {
+						*tab = Tab::Groups;
+					}
+				});
 				
-				multiplayer_entities(ui, world);
+				ui.separator();
+				
+				match *tab {
+					Tab::Entities => multiplayer_entities_ui(ui, world),
+					Tab::Groups => groups_ui(ui, world),
+				}
 				
 				ui.allocate_space(ui.available_size());
 			});
